02d6888e27aa157414ebf701d71b25166c595180
code-vedant <162135022+code-vedant@users.noreply.github.com>
2024-09-29 20:32:31 +0530

    csp

--- src/app.js
+++ src/app.js
@@ -4,10 +4,12 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+const allowedOrigins = ['https://yutubapp.vercel.app'];
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: allowedOrigins,
     credentials: true
-}))
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
