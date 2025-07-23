17bdf71e54b96f4a065d967483b1095b7d1599d0
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-07-23 10:37:51 +0530

    token changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -103,17 +103,18 @@ const loginUser = asyncHandler(async (req, res) => {
   }
 
   const accessToken = generateAccessToken(user._id, res);
+  
   const loggedInUser = await User.findById(user._id).select("-password");
 
   return res.status(200).json(new apiResponse(200, { user: loggedInUser, accessToken }, "User logged in successfully"));
 });
 
 const logoutUser = asyncHandler(async (req, res) => {
-  await User.findByIdAndUpdate(req.user._id, { $set: { refreshToken: undefined } });
+  await User.findByIdAndUpdate(req.user._id, { $set: { accessToken: undefined } });
 
   const options = { httpOnly: true, secure: true };
 
-  return res.status(200).clearCookie("accessToken", options).clearCookie("refreshToken", options).json(new apiResponse(200, {}, "User logged out successfully"));
+  return res.status(200).clearCookie("accessToken", options).json(new apiResponse(200, {}, "User logged out successfully"));
 });
 
 const changeCurrentPassword = asyncHandler(async (req, res) => {
--- src/utils/generateAccessToken.js
+++ src/utils/generateAccessToken.js
@@ -1,13 +1,13 @@
 import jwt from 'jsonwebtoken';
 
 const generateAccessToken = (userId,res) => {
-    const token = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn : "3d"} );
+    const token = jwt.sign({userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn : "7d"} );
 
     res.cookie("accessToken",token,{
         httpOnly : true,
         secure : process.env.NODE_ENV !== "development",
         sameSite : "strict",
-        maxAge : 3 * 24 * 60 * 60 * 1000
+        maxAge : 7 * 24 * 60 * 60 * 1000
     });
 
     return token;
