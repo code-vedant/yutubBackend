2c809a8156d7b2995037dc02f6eeb57b2463cf2c
code-vedant <162135022+code-vedant@users.noreply.github.com>
2025-07-30 12:08:35 +0530

    subscribers feature fix and updates

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,19 +1,16 @@
-import { Router } from 'express';
+import { Router } from "express";
 import {
-    getSubscribedChannels,
-    getUserChannelSubscribers,
-    toggleSubscription,
-} from "../controllers/subscription.controller.js"
-import {verifyJWT} from "../middlewares/auth.middleware.js"
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from "../controllers/subscription.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
-router.use(verifyJWT); // Apply verifyJWT middleware to all routes in this file
 
-router
-    .route("/c/:channelId")
-    .get(getUserChannelSubscribers)
-    .post(toggleSubscription);
-
+router.route("/c/:channelId").get(getUserChannelSubscribers);
 router.route("/u/:subscriberId").get(getSubscribedChannels);
+router.use(verifyJWT);
+router.route("/c/:channelId").post(toggleSubscription);
 
-export default router
+export default router;
